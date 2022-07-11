7ebd367a38bacdefb78af99d024e0cb7cdeec707
zlog-in <mrzz@tuta.io>
2022-07-11 22:21:51 +0200

    json file ignored

--- benchmark/mpc/ben.sh
+++ benchmark/mpc/ben.sh
@@ -1,8 +1,8 @@
 source /root/.cargo/env
 cd /home/narwhal/benchmark/
-git checkout benchmarking
-git restore .committee.json
-git pull
+#it checkout benchmarking
+#git restore .committee.json
+#git pull
 #rm logs/*.log
 
 fab local
