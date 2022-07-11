2f1d45b9f6df94cb6d32bb8bfc95ce286b8fd223
zlog-in <mrzz@tuta.io>
2022-07-11 15:49:15 +0200

    no new committee

--- benchmark/benchmark/local.py
+++ benchmark/benchmark/local.py
@@ -64,8 +64,7 @@ class LocalBench:
             key_files = [PathMaker.key_file(i) for i in range(nodes)]
             for filename in key_files:
                 cmd = CommandMaker.generate_key(filename).split()
-                if local == 1:
-                    subprocess.run(cmd, check=True)
+                #subprocess.run(cmd, check=True)
                 keys += [Key.from_file(filename)]
             node_i = int(subprocess.check_output(['tail', '-1', 'index.txt']))
             node_ip = '127.0.0.1'
