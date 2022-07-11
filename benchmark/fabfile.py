a4db9dc617713330225e7cd009cb56be3fbe194a
zlog-in <mrzz@tuta.io>
2022-07-11 20:53:41 +0200

    60s for 30 replicas

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -18,7 +18,7 @@ def local(ctx, debug=True):
         'workers': 1,
         'rate': 30_000,
         'tx_size': 512,
-        'duration': 20,
+        'duration': 60,
         'replicas': 10,
         'servers': 10,
         'local': 0,
